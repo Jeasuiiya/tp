ea784037b8a1c701de4c99576baf88a5444440c4
yiguangzheng <yiklek.me@gmail.com>
2023-01-07 15:12:48 +0000

    include. cmake

--- ccsrc/DistributedIR/a.cc
+++ ccsrc/DistributedIR/a.cc
@@ -1,4 +1,4 @@
-#include <graph.hpp>
-#include <server.hpp>
-#include <cluster.hpp>
+#include <DistributedIR/graph.hpp>
+#include <cluster/server.hpp>
+#include <cluster/cluster.hpp>
 int add(int a, int b) { return a + b; }
--- cmake/HeaderOnlyLibrary.cmake
+++ cmake/HeaderOnlyLibrary.cmake
@@ -1,7 +1,12 @@
 include(CMakeParseArguments)
+# add_header_only_library(target build_include_dir install_include_dir [DEPENDS <targets>] [ALIAS <alias_name>])
 function(add_header_only_library target_name build_interface install_interface)
-    cmake_parse_arguments(_ARG "" "" "DEPENDS" ${ARGN})
-    add_library(${target_name} INTERFACE)
+    cmake_parse_arguments(_ARG "" "ALIAS" "DEPENDS" ${ARGN})
+    if(_ARG_ALIAS)
+        add_library(${target_name} INTERFACE ALISA ${_ARG_ALIAS})
+    else()
+        add_library(${target_name} INTERFACE)
+    endif()
     target_include_directories(
         ${target_name}
         INTERFACE
