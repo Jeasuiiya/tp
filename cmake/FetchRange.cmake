f15286f6ad6f8bc5a22eabcd4f31fd2ddd9fcdec
易广政 <yelgors@163.com>
2023-03-14 20:35:42 +0800

    fix: cpm range-v3

--- cmake/FetchRange.cmake
+++ cmake/FetchRange.cmake
@@ -5,6 +5,7 @@ include(CPM)
 function(fetch_range)
   CPMAddPackage(
     NAME range
-    URL "https://github.com/ericniebler/range-v3/archive/541b06320b89c16787cc6f785f749f8847cf2cd1.zip" # Commits on Mar 14, 2023
+    # Commits on Mar 14, 2023
+    URL "https://github.com/ericniebler/range-v3/archive/541b06320b89c16787cc6f785f749f8847cf2cd1.zip"
     EXCLUDE_FROM_ALL ON)
 endfunction(fetch_range)
